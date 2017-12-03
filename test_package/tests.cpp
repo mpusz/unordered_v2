df8e902f8e3496b6bcf2f9406bbdad90ccf623ee
Mateusz Pusz <mateusz.pusz@gmail.com>
2017-12-03 17:45:13 +0100

    Heterogenous lookup support added for map and set

--- test_package/tests.cpp
+++ test_package/tests.cpp
@@ -20,59 +20,27 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "experimental/unordered_map"
-#include <unordered_map>
+#include "unordered_map"
+#include "unordered_set"
 #include <string>
 #include <string_view>
 #include <vector>
-
-//#include "hopscotch_map.h"
-//#include "inplace_string.h"
-
+#include <map>
+#include <iostream>
 
 using namespace std::literals;
 
 namespace {
 
- struct string_hash {
-   using hash_type = std::hash<std::string_view>;
+struct string_hash {
+  using is_transparent = void;                        // I confirm I know what I am doing
+  using hash_type = std::hash<std::string_view>;      // helper local type
+  size_t operator()(const std::string_view txt) const { return hash_type{}(txt); }
+  size_t operator()(const std::string& txt) const     { return hash_type{}(txt); }
+  size_t operator()(const char* txt) const            { return hash_type{}(txt); }
+};
 
-   size_t operator()(const std::string_view txt) const { return hash_type{}(txt); }
-   size_t operator()(const std::string& txt) const { return hash_type{}(txt); }
-   size_t operator()(const char* txt) const { return hash_type{}(txt); }
-
-//    template<size_t MAX_SIZE>
-//    size_t operator()(const mp::inplace_string<MAX_SIZE>& txt) const
-//    {
-//      return std::hash<std::string_view>{}(txt);
-//    }
-
-  };
-
-  struct string_equal {
-    using is_transparent = void;
-
-    template<typename L, typename R>
-    constexpr bool operator()(const L& lhs, const R& rhs) const
-    {
-      return std::string_view{lhs} == std::string_view{rhs};
-    }
-  };
-
-//  using user_id = mp::inplace_string<16>;
-//  using user_map = tsl::hopscotch_map<user_id, int, string_hash, string_equal>;
-//  using users = std::unordered_map<id, int, string_hash, string_equal>;
 }
-//
-//TEST(unordered, NoNeedToRecalculateHash)
-//{
-//  std::vector<user_map> products;
-//  const auto id = "abcd";
-//  const auto userHash = string_hash{}(id);
-//  for(auto& users : products) {
-//    users.find(id, userHash);
-//  }
-//}
 
 
 template<typename UnorderedMap>
@@ -81,7 +49,7 @@ void test_1()
   UnorderedMap map;
   map.find("abcd");      // const char *
   map.find("abcd"s);     // std::string
-//  map.find("abcd"sv);    // std::string_view
+  map.find("abcd"sv);    // std::string_view
 //  map.find(user_id{"abcd"});  // mp::inplace_string
 }
 
@@ -99,9 +67,9 @@ void test_2()
 
 int main()
 {
-  test_1<std::unordered_map<std::string, int>>();
-  test_1<std::experimental::unordered_map<std::string, int>>();
+//  test_1<std::unordered_map<std::string, int>>();
+  test_1<std::unordered_map<std::string, int, string_hash, std::equal_to<>>>();
 
-  test_2<std::unordered_map<std::string, int>>();
-  test_2<std::experimental::unordered_map<std::string, int>>();
+//  test_2<std::unordered_map<std::string, int>>();
+  test_2<std::unordered_map<std::string, int, string_hash, std::equal_to<>>>();
 }
